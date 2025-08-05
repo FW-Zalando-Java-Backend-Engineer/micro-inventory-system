2df22b0f0095f72d786e2b7c6362989171560675
safwan-kher <eng.safwan.kherallah@gmail.com>
2025-08-05 13:30:36 +0200

    fixed issues with inventory client class to add the zoken to the headers

--- product-service/src/main/java/org/example/product/service/InventoryClient.java
+++ product-service/src/main/java/org/example/product/service/InventoryClient.java
@@ -1,10 +1,14 @@
 package org.example.product.service;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 /**
  * HTTP client to communicate with the inventory-service.
@@ -24,6 +28,20 @@ public class InventoryClient {
     public record InventoryRequest(Long productId, int quantity) {}
 
 
+    private HttpHeaders getAuthHeaders() {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest currentRequest = attributes.getRequest();
+
+        String token = currentRequest.getHeader("Authorization");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization", token);
+
+        return headers;
+    }
+
+
     /**
      * Fetch current stock quantity for a product.
      *
@@ -31,11 +49,21 @@ public class InventoryClient {
      * @return quantity in stock
      */
     public int getStockQuantity(Long productId){
-        Integer quantity = restTemplate.getForObject(
-                inventoryServiceUrl  + productId,
+
+        HttpEntity<Void> entity = new HttpEntity<>(getAuthHeaders());
+        ResponseEntity<Integer> response = restTemplate.exchange(
+                inventoryServiceUrl + productId,
+                HttpMethod.GET,
+                entity,
                 Integer.class
         );
-        return quantity != null ? quantity : 0;
+        return response.getBody() != null ? response.getBody() : 0;
+
+//        Integer quantity = restTemplate.getForObject(
+//                inventoryServiceUrl  + productId,
+//                Integer.class
+//        );
+       // return quantity != null ? quantity : 0;
     }
 
     /**
@@ -45,10 +73,18 @@ public class InventoryClient {
      * @param quantity  initial stock
      */
     public void createInventory(Long productId, int quantity){
-        restTemplate.postForObject(
+        HttpEntity<InventoryRequest> entity = new HttpEntity<>(new InventoryRequest(productId, quantity), getAuthHeaders());
+
+        restTemplate.exchange(
                 inventoryServiceUrl,
-                new InventoryRequest(productId, quantity),
+                HttpMethod.POST,
+                entity,
                 Void.class
-                );
+        );
+//        restTemplate.postForObject(
+//                inventoryServiceUrl,
+//                new InventoryRequest(productId, quantity),
+//                Void.class
+//                );
     }
 }
